e62594a402abc9da87ece07cdbc677f7bdc8ae70
Claire Chen <claire.chen@ubisoft.com>
2022-02-20 14:21:16 -0600

    feat: reorganize keyboard

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <v-row align="center" justify="space-around">
-      <Key v-bind:keyValue="0" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="1" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="2" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="3" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="4" v-on:key-click="onKeyClicked" />
+      <Key v-bind:keyValue="5" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="'X'" v-on:key-click="onKeyClicked" />
     </v-row>
     <v-row align="center" justify="space-around">
-      <Key v-bind:keyValue="5" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="6" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="7" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="8" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="9" v-on:key-click="onKeyClicked" />
+      <Key v-bind:keyValue="0" v-on:key-click="onKeyClicked" />
       <Key v-bind:keyValue="'V'" v-on:key-click="onKeyClicked" />
     </v-row>
   </div>
