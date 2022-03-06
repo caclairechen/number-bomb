3321b20792a343f510500ab1be00e5d455601c7d
Claire Chen <claire.chen@ubisoft.com>
2022-03-06 12:12:12 -0600

    move server to a different project

--- src/services/socketio.service.js
+++ src/services/socketio.service.js
@@ -1,11 +1,10 @@
 import { io } from "socket.io-client";
-
 class SocketioService {
   socket;
   constructor() {}
 
   setupSocketConnection() {
-    this.socket = io("http://localhost:3000");
+    this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT);
   }
 
   disconnect() {
